5c3683abebe48c4f1e8f37b48a85147118d7a94a
ravirajuv <35996663+ravirajuv@users.noreply.github.com>
2018-08-22 12:19:10 +0530

    Add files via upload

---
+++ twitter_data.py
@@ -0,0 +1,15 @@
+import boto3
+import json
+import logging
+import urllib
+import time
+import codecs
+import tweepy
+
+
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    
+    return 'Hello from Lambda'
